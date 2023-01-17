a1eaa6677aa18784e611274f4b21aadf7e12a554
Brody <brodypen7987@gmail.com>
2023-01-16 19:38:49 -0600

    Work on gameboard

--- src/factory/__tests__/gameboardFactory.test.ts
+++ src/factory/__tests__/gameboardFactory.test.ts
@@ -1,10 +1,13 @@
 // Step 2 of TOP
 // x axis is A-J, y axis is 1-10
 import Gameboard from "../gameboardFactory";
+import Ship from "../shipFactory";
 describe("Gameboard testing", () => {
     let gameboard: Gameboard;
+    let ship: Ship;
     beforeEach(() => {
         gameboard = new Gameboard();
+        ship = new Ship(4)
     });
 
     test("Gameboard is created", () => {
@@ -13,12 +16,12 @@ describe("Gameboard testing", () => {
     // placeShip testing
     test("Placed ship at right location horizontally", () => {
         // place ship 0 at 0,0 horizontally.
-        gameboard.placeShip(0, 0, 0, true);
+        gameboard.placeShip(ship, 0, 0, true);
         expect(gameboard.ships[0].location).toEqual([[0, 0], [1, 0], [2, 0], [3, 0]]);
     });
     test("Placed ship at right location vertically", () => {
       // place ship 0 at 0,0 vertically.
-      gameboard.placeShip(0, 0, 0, false);
+      gameboard.placeShip(ship, 0, 0, false);
       expect(gameboard.ships[0].location).toEqual([
         [0, 0],
         [0, 1],
@@ -27,22 +30,29 @@ describe("Gameboard testing", () => {
       ]);
     });
     test("ReceiveAttack hit", () => {
-        gameboard.placeShip(0, 0, 0, true);
+        gameboard.placeShip(ship, 0, 0, true);
         gameboard.receiveAttack(0, 0);
         expect(gameboard.ships[0].hits).toEqual([1]);
     }
     );
     test("ReceiveAttack miss", () => {
-      gameboard.placeShip(0, 0, 0, true);
+      gameboard.placeShip(ship, 0, 0, true);
       gameboard.receiveAttack(5, 5);
       expect(gameboard.ships[0].hits).toEqual([0]);
     });
     test("All ships sunk", () => {
-        gameboard.placeShip(0, 0, 0, true);
+        gameboard.placeShip(ship, 0, 0, true);
         gameboard.receiveAttack(0, 0);
         gameboard.receiveAttack(1, 0);
         gameboard.receiveAttack(2, 0);
         gameboard.receiveAttack(3, 0);
         expect(gameboard.allShipsSunk()).toBe(true);
-        }
+        });
+    test("Ships are not sunk", () => {
+          gameboard.placeShip(ship, 0, 0, true);
+          gameboard.receiveAttack(0, 0);
+          gameboard.receiveAttack(1, 0);
+          gameboard.receiveAttack(2, 0);
+          expect(gameboard.allShipsSunk()).toBe(false);
+    });
 });
--- src/factory/gameboardFactory.ts
+++ src/factory/gameboardFactory.ts
@@ -1,3 +1,28 @@
-class Gameboard {}
+import Ship from "./shipFactory";
+class Gameboard {
+    board: object[][];
+    ships: Ship[];
+    missedAttacks: number[][];
+    constructor() {
+        this.board = [];
+        this.ships = [];
+        this.missedAttacks = [];
+    }
+    init(){
+        let fakeColumnBoard: object[] = [];
+        for (let i = 0; i < 10; i++) {
+        fakeColumnBoard.push({hasShip: false, isShot: false });
+        }
+        for (let i = 0; i < 10; i++) {
+            this.board.push(fakeColumnBoard);
+        }
+    }
+
+    placeShip(ship: Ship, x: number, y: number, horizontal: boolean) {
+        // if (horizontal) {
+        //     for (let i = 0; i < ship.length; i++) {
+        //         ship.location.push([
+    }
+}
 
 export default Gameboard;
