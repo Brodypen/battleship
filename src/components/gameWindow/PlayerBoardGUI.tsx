62161556054fd99f381c3d6802fee07a430471ea
Brody <brodypen7987@gmail.com>
2023-01-27 09:32:06 -0600

    Add logic to rotate ship

--- src/components/gameWindow/PlayerBoardGUI.tsx
+++ src/components/gameWindow/PlayerBoardGUI.tsx
@@ -1,47 +1,58 @@
-import React from 'react'
-import Gameboard from '../../factory/gameboardFactory'
-interface GameProps{
-  playerBoard: Gameboard
+import React from "react";
+import Gameboard from "../../factory/gameboardFactory";
+interface GameProps {
+  playerBoard: Gameboard;
 }
 
-const PlayerBoardGUI = ({playerBoard}: GameProps) => {
+const PlayerBoardGUI = ({ playerBoard }: GameProps) => {
+  const [shipToMove, setShipToMove] = React.useState(-1);
+  const [displayWords, setDisplayWords] = React.useState(
+    "Click to rearrange your ships!"
+  );
 
-  const [shipToMove, setShipToMove] = React.useState(-1)
-  const [displayWords, setDisplayWords] = React.useState("Click to rearrange your ships!")
-
-  const shipNames: {[key: number]: string}  = {
+  const shipNames: { [key: number]: string } = {
     0: "Destroyer",
     1: "Submarine",
     2: "BattleShip",
     3: "Carrier",
-    4: "Cruiser"
-  }
+    4: "Cruiser",
+  };
 
   const onMove = (Row: number, Col: number) => {
     const shipPicked = playerBoard.getBoard[Row][Col].hasShip;
     if (shipToMove === -1 && shipPicked === -1) return;
-       
-    
+
     // console.log(`Row: ${Row} Col: ${Col}`);
     // console.log("SP:", shipPicked);
     // console.log("Ship to move", shipToMove);
-     if (shipPicked !== -1 && shipToMove !== shipPicked) {
-       setShipToMove(shipPicked);
-        setDisplayWords("Ship " + shipNames[`${shipPicked}`] + " Selected!")
-       return;
-     }
-     if(playerBoard.isValidPlacement(playerBoard.getShips[shipToMove], Row, Col) === false){ 
-        setDisplayWords("Invalid Placement!")
-      return};
-    
+    if (shipPicked !== -1 && shipToMove !== shipPicked) {
+      setShipToMove(shipPicked);
+      setDisplayWords("Ship " + shipNames[`${shipPicked}`] + " Selected!");
+      return;
+    }
     const x = playerBoard.getShips[shipToMove].getShipHead[0];
     const y = playerBoard.getShips[shipToMove].getShipHead[1];
+    if (
+      playerBoard.isValidPlacement(
+        playerBoard.getShips[shipToMove],
+        Row,
+        Col
+      ) === false
+    ) {
+      if (
+        JSON.stringify([x, y]) ===
+        JSON.stringify([Row, Col])
+      ) {
+        setDisplayWords("Rotating Ship!");
+      } else {
+        setDisplayWords("Invalid Placement!");
+      }
+      return;
+    }
+
     playerBoard.relocateShip(playerBoard.getShips[shipToMove], Row, Col);
     setShipToMove(-1);
-     
-    
-
-  }
+  };
   return (
     <div className="gap-5">
       <p
@@ -107,6 +118,6 @@ const PlayerBoardGUI = ({playerBoard}: GameProps) => {
       </div>
     </div>
   );
-}
+};
 
-export default PlayerBoardGUI
+export default PlayerBoardGUI;
